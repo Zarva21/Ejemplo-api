f70f87511a0e3709386308d5b2ca1626f1beaf2d
Zarva21 <165863841+Zarva21@users.noreply.github.com>
2024-08-26 20:38:58 -0600

    Nueva conexion

--- app/config/env.js
+++ app/config/env.js
@@ -1,8 +1,8 @@
 const env = {
     database: 'db_clases_progra',
     username: 'db_clases_progra_user',
-    password: 'Epzitgs51D4lnn3LaYWkIuyVi8mvFxBX',
-    host: 'dpg-cqc60ft6l47c73cuht80-a.oregon-postgres.render.com',
+    password: '9zeStIkHTKZPnKK3ELEhXmcv5rVcZxf4',
+    host: 'dpg-cr6jn5ij1k6c73d5epo0-a.oregon-postgres.render.com',
     port: '5432',
     dialect: 'postgres',
 
