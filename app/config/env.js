354f9d0df0607167f19775d068d9ac763b8cb722
Zarva21 <165863841+Zarva21@users.noreply.github.com>
2024-08-26 20:52:15 -0600

    cambio de datos

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'db_clases_progra_tdsy',
     username: 'db_clases_progra_user',
     password: '9zeStIkHTKZPnKK3ELEhXmcv5rVcZxf4',
-    host: 'dpg-cr6jn5ij1k6c73d5epo0-a.oregon-postgres.render.com',
+    host: 'dpg-cr6jn5ij1k6c73d5epo0-a',
     port: '5432',
     dialect: 'postgres',
 
